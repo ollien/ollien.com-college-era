0b6038c17178cd29d286c1a3248ed34930032260
Nick Krichevsky <njk828@gmail.com>
2015-01-18 01:48:22 -0500

    Remove unecessary functions of github script

--- ollienHomepage/githubScreenshots.py
+++ ollienHomepage/githubScreenshots.py
@@ -15,26 +15,3 @@ class GithubScreenshots(object):
         if r.status_code==200:
             return json.loads(r.text)
         return r.status_code
-    
-    def getCommits(self,user,repoName):
-        r = requests.get("https://api.github.com/repos/%s/%s/commits"%(user,repoName),headers=self.header)
-        if r.status_code==200:
-            return json.loads(r.text)
-        return r.status_code
-        
-    def getCommit(self,user,repoName,hash):
-        r = requests.get("https://api.github.com/repos/%s/%s/commits/%s"%(user,repoName,hash),headers=self.header)
-        if r.status_code==200:
-            return json.loads(r.text)
-        return r.status_code
-    def getChangedFiles(self,commitLog=None,user=None,repoName=None,hash=None):
-        if commitLog != None:
-            return [item['filename'] for item in commitLog['files']]
-        elif user != None and repoName != None and hash !=None:
-            r = requests.get("https://api.github.com/repos/%s/%s/commits/%s"%(user,repoName,hash),headers=self.header)
-            if r.status_code==200:
-                result = json.loads(r.text)
-                return [item['filename'] for item in result['files']]
-            return r.status_code
-        return None            
-        
