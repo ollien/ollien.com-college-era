58f35abeec2f7a10d716e7a5e28a255763caa396
Nick Krichevsky <njk828@gmail.com>
2015-01-23 12:30:48 -0500

    Add auto fading

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+var canFade = true;
 function ready(){
 	var dots = Array.prototype.slice.call(document.querySelectorAll(".circle"));
 	var rawDisplays = Array.prototype.slice.call(document.querySelectorAll(".display"));
@@ -8,27 +9,19 @@ function ready(){
 	});
 	console.log(displays);
 	dots.forEach(function(item){
-	    item.onclick = function(){
-		    var currentDisplay = displays[currentIndex];
-		    console.log(currentDisplay);
-		    if (currentIndex != item.getAttribute("num")){
+			item.onclick = function(){
+				var currentDisplay = displays[currentIndex];
+				canFade=false;
+				if (currentIndex != item.getAttribute("num")){
 				currentIndex = item.getAttribute("num");
-				console.log(currentIndex);
 				var newDisplay = displays[currentIndex];
 				console.log(newDisplay);	
 				console.log(window.getComputedStyle(currentDisplay).getPropertyValue("transition-duration"));
-				currentDisplay.classList.add("faded")
-				setTimeout(function() {
-					currentDisplay.classList.add("hiddenDisplay");
-					newDisplay.classList.remove("hiddenDisplay");
-					setTimeout(function() {
-						newDisplay.classList.remove("faded");
-					}, timeStringtoTime(window.getComputedStyle(currentDisplay).getPropertyValue("transition-duration")));
-				}, timeStringtoTime(window.getComputedStyle(currentDisplay).getPropertyValue("transition-duration")));
-						
+				fade(currentDisplay,newDisplay);			
 			}
 		} 
-    });
+		});
+		autoFade(5000, dots, displays);
 }
 function timeStringtoTime(s){
 	if (s.indexOf('ms')!=-1){
@@ -40,4 +33,35 @@ function timeStringtoTime(s){
 		var result = s.replace('s','');
 		return parseFloat(result)*1000;
 	}
+}
+
+function autoFade(t,circles,displays,index){
+	if (canFade){
+		if (index!==undefined && index>=0) {
+			circles[index].classList.remove('selected');
+			startDisplay = displays[index.toString()];
+			if (index!=circles.length-1)
+				index+=1;
+			else
+				index=0;
+			endDisplay = displays[index.toString()];
+			fade(startDisplay,endDisplay);
+			circles[index].classList.add('selected');
+			
+		}
+		else
+			index=0;
+		setTimeout(function(){autoFade(t,circles,displays,index);}, t);
+	}
+}
+
+function fade(start,finish){
+	start.classList.add("faded")
+	setTimeout(function() {
+		start.classList.add("hiddenDisplay");
+		finish.classList.remove("hiddenDisplay");
+		setTimeout(function() {
+			finish.classList.remove("faded");
+		}, timeStringtoTime(window.getComputedStyle(start).getPropertyValue("transition-duration")));
+	}, timeStringtoTime(window.getComputedStyle(start).getPropertyValue("transition-duration")));
 }
