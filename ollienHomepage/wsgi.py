2f21edbd895dea24d065b1c41b57cc1930f40096
Nick Krichevsky <njk828@gmail.com>
2015-01-26 00:42:03 -0500

    Add Creative Commons License for homepage

--- ollienHomepage/wsgi.py
+++ ollienHomepage/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ollienHomepage.settings")
 
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ollienHomepage.settings")
 application = get_wsgi_application()
