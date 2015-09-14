56cda48a64d38ac56a7407f1a28d003aca338b50
Nick Krichevsky <njk828@gmail.com>
2015-09-14 16:46:09 -0400

    Fix bug where exceptions wouldn't be replaced

--- ollienHomepage/management/commands/getScreenshots.py
+++ ollienHomepage/management/commands/getScreenshots.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 			raise Exception("Github returned statuscode "+str(repos))
 		for repo in repos:
 			repoName = titlecase.titlecase(repo['name'])
-			for word in repoName:
+			for word in repoName.split("-"):
 				if word in exceptions:
 					repoName = repoName.replace(word, exceptions[word])
 			repoDesc = repo['description']
