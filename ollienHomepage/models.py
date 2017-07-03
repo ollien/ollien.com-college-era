01047612852ff0b83d89947d123e78a7d3513ee2
Nick Krichevsky <nick@ollien.com>
2017-07-03 18:59:58 -0400

    Remove site 1.0 assets and code

--- ollienHomepage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Repo(models.Model):
-    name=models.TextField()
-    description=models.TextField()
-    sha=models.TextField()
-    imagePath=models.TextField()
-    url=models.TextField()
