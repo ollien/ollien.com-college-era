5e3a054adb0f4c7f14d36308204bf2397635a16b
Nick Krichevsky <nick@ollien.com>
2022-08-28 15:04:04 -0400

    Update Gulp, fix up dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
-FROM nginx
+FROM nginx:1.23.1
 MAINTAINER Nick Krichevsky <nick@ollien.com>
 
-RUN apt-get update && apt-get install -y git curl gnupg && \
-	curl -sL https://deb.nodesource.com/setup_6.x | bash - && \
+RUN apt-get update && apt-get install -y git curl python gnupg && \
+	curl -sL https://deb.nodesource.com/setup_16.x | bash - && \
 	apt-get update && apt-get install -y nodejs && \
-	npm install -g check-dependencies gulp-cli && \
-	useradd -m ollien.com && \
-	mkdir /home/ollien.com/www
-COPY nginx/conf.d /etc/nginx/conf.d/default.conf
-COPY start.sh /
+	useradd -m ollien.com
 
-CMD /start.sh
+USER ollien.com
+COPY --chown=ollien.com:ollien.com . /home/ollien.com/src
+COPY nginx/conf.d /etc/nginx/conf.d/default.conf
+
+WORKDIR /home/ollien.com/src
+RUN npm install && \
+	npx gulp sass --prod --dest=/home/ollien.com/src/static/ && \
+	mkdir /home/ollien.com/www && \
+	cp -r /home/ollien.com/src/index.html /home/ollien.com/src/static /home/ollien.com/www/
+
+WORKDIR /home/ollien.com/www
+USER root
+
+CMD nginx -g "daemon off;"
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,27 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var sass = require("gulp-sass")(require("sass"));
 var autoprefixer = require("gulp-autoprefixer");
 var gulpif = require("gulp-if");
 var clean_css = require("gulp-clean-css");
 var yargs = require("yargs");
 
-gulp.task("default", ["sass"]);
-
-//Write scss to file. Use prod flog to minify css, or the dest flag to set the destination of the build
-gulp.task("sass", function() {
+// Write scss to file. Use prod flog to minify css, or the dest flag to set the destination of the build
+exports.sass = function() {
 	let dest = yargs.argv.hasOwnProperty("dest") ? yargs.argv.dest : "./";
 	return gulp.src("./index.scss")
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer({
-			browsers: [
-				"last 2 versions",
-				"ie >= 11"
-			],
 			cascade: false
 		}))
 		.pipe(gulpif(yargs.argv.hasOwnProperty("prod"), clean_css()))
 		.pipe(gulp.dest(dest));
-});
+};
 
-gulp.task("watch", function() {
+exports.watch = function() {
 	var sassWatch = gulp.watch("./index.scss", ["sass"])
 	sassWatch.on("change", function (event) {
 		console.log("Event '"+ event.type + "' detected on " + event.path + ". Running sass.");
 	});
-});
+};
+
+exports.default = exports.sass;
